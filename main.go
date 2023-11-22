c266430f9b43bc4afc7c6545df0e6b1cf90c8ab8
Rizqi Taufiq <rizqitaufiqf@gmail.com>
2023-11-22 15:07:54 +0700

    feat(user): create user repository

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"crowdfunding/user"
 	"fmt"
 	"github.com/joho/godotenv"
 	"gorm.io/driver/postgres"
@@ -10,10 +11,12 @@ import (
 )
 
 func main() {
+	// load env file
 	envErr := godotenv.Load()
 	if envErr != nil {
 		log.Fatal("Error loading .env file")
 	}
+	// setup env values to variable
 	host := os.Getenv("DATABASE_HOST")
 	username := os.Getenv("DATABASE_USERNAME")
 	password := os.Getenv("DATABASE_PASSWORD")
@@ -22,13 +25,27 @@ func main() {
 	sslMode := os.Getenv("DATABASE_SSL_ENABLED")
 	timezone := os.Getenv("TIMEZONE")
 
+	// generate connection string
 	dsn := fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%s sslmode=%s TimeZone=%s", host, username, password, databaseName, port, sslMode, timezone)
-	_, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	// connect to database
+	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
 
 	if err != nil {
 		log.Fatal(err.Error())
 	}
 
 	fmt.Println("Connection to database successfully")
+	userRepository := user.NewRepository(db)
+
+	usr := user.User{
+		ID:   "139d727b-bda9-4978-9cef-61767ee784c7",
+		Name: "rere",
+	}
+
+	save, err := userRepository.Save(usr)
+	if err != nil {
+		return
+	}
+	fmt.Println(save)
 
 }
---
+++ user/entity.go
@@ -0,0 +1,18 @@
+package user
+
+import "time"
+
+// User Model
+type User struct {
+	ID             string
+	Name           string
+	Occupation     string
+	Email          string
+	Role           string
+	PasswordHash   string
+	Token          string
+	AvatarFileName string
+	CreatedAt      time.Time
+	UpdatedAt      time.Time
+	DeletedAt      time.Time
+}
