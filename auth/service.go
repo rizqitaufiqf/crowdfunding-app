c3a1343462cfe2abcdf05de64b7889b4de4ff9b5
Rizqi Taufiq <rizqitaufiqf@gmail.com>
2023-11-23 16:57:05 +0700

    feat(auth): add validation jwt token

--- auth/service.go
+++ auth/service.go
@@ -1,6 +1,7 @@
 package auth
 
 import (
+	"errors"
 	"github.com/golang-jwt/jwt/v5"
 	"os"
 	"strconv"
@@ -46,4 +47,20 @@ func (s *jwtService) GenerateToken(userID string) (string, error) {
 	}
 
 	return signedToken, nil
+}
+
+func (s *jwtService) ValidateToken(encodedToken string) (*jwt.Token, error) {
+	secretKey := os.Getenv("JWT_AUTH_SECRET")
+	tokens, err := jwt.Parse(encodedToken, func(token *jwt.Token) (interface{}, error) {
+		if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
+			return nil, errors.New("invalid token")
+		}
+		return []byte(secretKey), nil
+	})
+
+	if err != nil {
+		return tokens, err
+	}
+
+	return tokens, err
 }
