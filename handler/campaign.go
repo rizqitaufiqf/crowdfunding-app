3df223104f8fe9cf55b11fdb90a802322938b37e
Rizqi Taufiq <rizqitaufiqf@gmail.com>
2023-11-28 15:38:15 +0700

    feat(campaign): format response get campaigns endpoint

--- handler/campaign.go
+++ handler/campaign.go
@@ -5,6 +5,7 @@ import (
 	"crowdfunding/helper"
 	"github.com/gin-gonic/gin"
 	"net/http"
+	"strings"
 )
 
 type campaignHandler struct {
@@ -18,12 +19,13 @@ func NewCampaignHandler(service campaign.Service) *campaignHandler {
 func (h *campaignHandler) GetCampaigns(c *gin.Context) {
 	userID := c.Query("user_id")
 	campaigns, err := h.service.GetCampaigns(userID)
-	if err != nil {
+	if err != nil && !strings.Contains(err.Error(), "invalid input syntax for type uuid") {
 		response := helper.APIResponse("Error to get campaigns", http.StatusBadRequest, "error", err.Error())
 		c.JSON(http.StatusBadRequest, response)
 		return
 	}
 
-	response := helper.APIResponse("List of campaigns", http.StatusOK, "success", campaigns)
+	formatter := campaign.FormatCampaigns(campaigns)
+	response := helper.APIResponse("List of campaigns", http.StatusOK, "success", formatter)
 	c.JSON(http.StatusOK, response)
 }
